392ad718defecc9edda9b2fbcdef11155e9c442b
Harsh Sharma <117378407+GitHub-Harsh-Sharma@users.noreply.github.com>
2023-03-06 11:28:11 +0530

    Add files via upload

---
+++ average.java
@@ -0,0 +1,11 @@
+class average
+{
+     public static void main(String[] args) {
+        int a,b,c,avg;
+        a=2;
+        b=3;
+        c=4;
+        avg=a+b+c/3;
+        System.out.print("The average is:"+avg);
+    }
+}
